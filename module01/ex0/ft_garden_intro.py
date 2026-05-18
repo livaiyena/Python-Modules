b670b341922d9d5e09b4a13aa62c346f68383606
livaiyena <apaydinkerem18@gmail.com>
2026-05-18 19:33:54 +0300

    ex0 intro to name main structure

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    name = "rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name.capitalize()}\nHeight: {height}cm\nAge: {age} days\n")
+    print("=== End of Program ===")
