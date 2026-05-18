2320e23ec0d61d15ec5979e116acbf404ac01d7e
livaiyena <apaydinkerem18@gmail.com>
2026-05-18 13:51:27 +0300

    Implement total harvest calculation logic

---
+++ module00/ex3/ft_harvest_total.py
@@ -0,0 +1,5 @@
+def ft_harvest_total() -> None:
+    harvestDay1 = int(input("Day 1 harvest: "))
+    harvestDay2 = int(input("Day 2 harvest: "))
+    harvestDay3 = int(input("Day 3 harvest: "))
+    print("Total harvest:", harvestDay1 + harvestDay2 + harvestDay3)
