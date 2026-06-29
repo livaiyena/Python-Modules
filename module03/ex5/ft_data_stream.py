d07fe0f3eea0884e7c26d3feabc9baea913f121d
livaiyena <apaydinkerem18@gmail.com>
2026-06-29 08:59:28 +0300

    feat(ft_data_stream): add generator-based event stream boilerplate

---
+++ module03/ex5/ft_data_stream.py
@@ -0,0 +1,18 @@
+import random
+from typing import Generator
+
+
+def gen_event() -> Generator:
+    pass
+
+
+def consume_event(event_list: list) -> Generator:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
