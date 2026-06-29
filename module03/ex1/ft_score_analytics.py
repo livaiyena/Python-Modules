31fa6dcc4e99242da4ef97252fbe6d4679fe6d0a
livaiyena <apaydinkerem18@gmail.com>
2026-06-29 08:59:27 +0300

    feat(ft_score_analytics): add score calculation and statistics from CLI args

---
+++ module03/ex1/ft_score_analytics.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def main() -> None:
+    print("=== Player Score Analytics ===")
+
+    _ , *scores_raw = sys.argv
+    scores = []
+    for score in scores_raw:
+        try:
+            temp = int(score)
+            scores = scores + [temp]
+        except ValueError:
+            print(f"Invalid parameter: '{score}'")
+    if not scores:
+        print("No scores provided. "
+            "Usage: python3 ft_score_analytics.py <score1> <score2> ...\n")
+        return
+    
+    print(f"Scores processed: {scores}")
+    print(f"Total players: {len(scores)}")
+    print(f"Total score: {sum(scores)}")
+    print(f"Average score: {sum(scores) / len(scores):.1f}")
+    print(f"High score: {max(scores)}")
+    print(f"Low score: {min(scores)}")
+    print(f"Score range: {max(scores) - min(scores)}\n")
+    
+if __name__ == "__main__":
+    main()
